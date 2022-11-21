121eb25dcaf3ed6626f50d0b5bca7dc4f71c84f3
David Chamont <chamont@in2p3.fr>
2022-11-21 18:03:03 +0100

    So to not run as root any more.

--- bin/run.sh
+++ bin/run.sh
@@ -4,6 +4,6 @@
 #shift
 #flavor=${1}
 #shift
-#docker run -it --rm -v ${PWD}:/work -w /work `cat ${SCRIPT_DIR}/name.${target}.${flavor}.txt` $*
+#docker run --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work `cat ${SCRIPT_DIR}/name.${target}.${flavor}.txt` $*
 
-docker run -it --rm -v ${PWD}:/work -w /work `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
+docker run --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
