cdf9a52b860c665047974c7ec64f2918543a0b67
David Chamont <chamont@in2p3.fr>
2022-12-01 16:16:49 +0100

    Un peu de nettoyage

--- bin/run-intel.sh
+++ bin/run-intel.sh
@@ -7,13 +7,10 @@ then
 fi
 export DEV_SCRIPTS_DOCKER_DIR=`cat /tmp/dev-scripts-recipe-dir-$PPID`
 
-#docker run --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
-
 # Pour que sycl-ls voit l'iGPU, il faut --privileged, et apparemment laisser le user root
 # Par ailleurs, selon :
 #   https://www.intel.com/content/www/us/en/develop/documentation/get-started-with-intel-oneapi-base-linux/top/using-containers/using-containers-with-the-command-line.html
-# Il faudrait --device=/dev/dri, mais je n'ai pas vu d'effet : avec ou sans, je vois l'iGPU,
-# mais je ne vois pas le dGPU de NVidia. D'apres un gars d'intel, --device=/dev/dri peut etre indispensable
-# pour certaines applications (un simple sycl-ls ne teste que peu de choses)
+# Il faudrait --device=/dev/dri, mais je n'ai pas vu d'effet : d'apres un gars d'intel, --device=/dev/dri peut etre indispensable
+# pour certaines applications (un simple sycl-ls ne teste que peu de choses). Lui-même met carrément -v /dev:/dev .
 
 docker run --privileged -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
--- bin/run-nvidia.sh
+++ bin/run-nvidia.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#target=${1}
-#shift
-#flavor=${1}
-#shift
-#docker run --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work `cat ${SCRIPT_DIR}/name.${target}.${flavor}.txt` $*
-
 if [ ! -r /tmp/dev-scripts-recipe-dir-$PPID ]
 then
   echo unknown recipe
   exit
 fi
 export DEV_SCRIPTS_DOCKER_DIR=`cat /tmp/dev-scripts-recipe-dir-$PPID`
+
+# Côté hôte, il faut avoir installer une extension NVidia.
+# Par exemple, sur ubuntu 22.04 : `apt install nvidia-docker2`.
+# Une des difficulté que je ne maitrise pas : il faut avoir la meme version
+# de "driver" entre hôte et conteneur.
+# Pour utiliser l'image oneapi basée sur ubuntu 20.4, en latest,
+# j'ai du installer sur l´hôte nvidia-driver-520
 
 docker run --gpus all --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
