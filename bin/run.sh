53d2a113f6f8aa146635281a7dbbdf6c15ae8845
David Chamont <chamont@in2p3.fr>
2023-01-20 12:10:26 +0100

    Add a -8 option so to swith on the -p 8888:8888

--- bin/run.sh
+++ bin/run.sh
@@ -5,21 +5,25 @@ Help()
    # Display Help
    echo "Start a container from the current selected image."
    echo
-   echo "Syntax: run.sh [-u|-h]"
+   echo "Syntax: run.sh [-h|-u|-8]"
    echo "options:"
    echo "h     Print this Help."
    echo "u     Run with --user $(id -u):$(id -g)."
+   echo "8     Run with -p 8888:8888."
    echo
 }
 
 # Parse the options
-while getopts ":hu" option; do
+while getopts ":hu8" option; do
    case $option in
       h) # display Help
          Help
          exit;;
       u) # force rebuilding
-         export DEV_SCRIPTS_RUN_USER=on
+         export DEV_SCRIPTS_RUN_USER="on"
+         ;;
+      8) # forward port 8888
+         export DEV_SCRIPTS_RUN_8888="-p 8888:8888"
          ;;
    esac
 done
@@ -35,7 +39,7 @@ export DEV_SCRIPTS_DOCKER_DIR=`cat /tmp/dev-scripts-recipe-dir-$PPID`
 # Main docker command
 if [ -z "${DEV_SCRIPTS_RUN_USER}" ]
 then
-  docker run -p 8888:8888 -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
+  docker run ${DEV_SCRIPTS_RUN_8888} -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
 else
-  docker run -p 8888:8888 --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
+  docker run ${DEV_SCRIPTS_RUN_8888} --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
 fi
