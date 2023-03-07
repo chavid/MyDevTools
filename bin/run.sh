0f68c9407feb046f9d2ab00d63803432210c814f
David Chamont <chamont@in2p3.fr>
2023-03-07 23:57:46 +0100

    option x11

--- bin/run.sh
+++ bin/run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# The DISPLAY export require you to predefine MYIP
+# and is tested only for MacOSX. Having something which
+# works also with linux and windows still TO BE DONE.
+# So to find your IP number on MacOS, use command
+# ifconfig and search for inet lines.
+
 Help()
 {
    # Display Help
    echo "Start a container from the current selected image."
    echo
-   echo "Syntax: run.sh [-h|-u|-8]"
+   echo "Syntax: run.sh [-h|-u|-8|-x]"
    echo "options:"
    echo "h     Print this Help."
    echo "u     Run with --user $(id -u):$(id -g)."
    echo "8     Run with -p 8888:8888."
+   echo "x     Run with X11 tunelling."
    echo
 }
 
 # Parse the options
-while getopts ":hu8" option; do
+while getopts ":hu8x" option; do
    case $option in
       h) # display Help
          Help
@@ -24,6 +31,10 @@ while getopts ":hu8" option; do
          ;;
       8) # forward port 8888
          export DEV_SCRIPTS_RUN_8888="-p 8888:8888"
+         ;;
+      x) # forward X11
+         xhost + local:docker
+         export DEV_SCRIPTS_RUN_X11="-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro"
          ;;
    esac
 done
@@ -39,7 +50,7 @@ export DEV_SCRIPTS_DOCKER_DIR=`cat /tmp/dev-scripts-recipe-dir-$PPID`
 # Main docker command
 if [ -z "${DEV_SCRIPTS_RUN_USER}" ]
 then
-  docker run ${DEV_SCRIPTS_RUN_8888} -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
+  docker run ${DEV_SCRIPTS_RUN_8888} ${DEV_SCRIPTS_RUN_X11} -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
 else
-  docker run ${DEV_SCRIPTS_RUN_8888} --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
+  docker run ${DEV_SCRIPTS_RUN_8888} ${DEV_SCRIPTS_RUN_X11} --user "$(id -u):$(id -g)" -it --rm -v ${PWD}:/work -w /work -e DTAG=`cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag` `cat ${DEV_SCRIPTS_DOCKER_DIR}/Dockertag`
 fi
